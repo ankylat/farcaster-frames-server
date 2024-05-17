3e3477686b7ba50ba98d29819356a66fff488d72
anky <kithkui@gmail.com>
2024-05-17 16:00:59 -0400

    aloja

--- routes/farhack/index.js
+++ routes/farhack/index.js
@@ -489,7 +489,7 @@ router.post("/bot", async (req, res) => {
     let botResponse;
     const fullUrl = req.protocol + "://" + req.get("host");
     const fullAddUrl = `https://warpcast.com/~/add-cast-action?url=${encodeURIComponent(`${process.env.SERVER_API_ROUTE}/invokeanky`)}`;
-    res.setHeader("Content-Type", "text/html");
+    return res.setHeader("Content-Type", "text/html");
       res.status(200).send(`
       <!DOCTYPE html>
       <html>
